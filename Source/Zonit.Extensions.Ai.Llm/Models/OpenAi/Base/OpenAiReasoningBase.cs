42d6991fa1d483e78dc527c645fa6ae1394ab46a
Dawid Haładus <alerinpoland@gmail.com>
2025-07-03 23:37:46 +0200

    Zaktualizowano wersję pakietu i rozszerzono klasę
    W pliku `Directory.Packages.props` zaktualizowano wersję zestawu z `1.0.5` na `1.0.6`, co sugeruje wprowadzenie poprawek lub nowych funkcji.
    
    W pliku `OpenAiReasoningBase.cs` dodano interfejs `ITextLlmBase` do klasy `OpenAiReasoningBase`, co rozszerza jej funkcjonalność, umożliwiając implementację dodatkowych metod lub właściwości.

--- Source/Zonit.Extensions.Ai.Llm/Models/OpenAi/Base/OpenAiReasoningBase.cs
+++ Source/Zonit.Extensions.Ai.Llm/Models/OpenAi/Base/OpenAiReasoningBase.cs
@@ -1,6 +1,6 @@
 ﻿namespace Zonit.Extensions.Ai.Llm;
 
-public abstract class OpenAiReasoningBase : OpenAiBase
+public abstract class OpenAiReasoningBase : OpenAiBase, ITextLlmBase
 {
     public virtual decimal? PriceCachedInput { get; } = null;
 
